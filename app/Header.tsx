0ed1ee2d875842c6b522190d76974b947fab808e
nalendro16 <nalendro_agung@yahoo.com>
2022-11-24 23:33:37 +0700

    feat: add router why so easy:(

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
-  return <div className='bg-neutral-200 font-semibold'>This Header</div>
+  return (
+    <div className='bg-blue-500 font-semibold flex flex-row-reverse justify-between px-20 py-4'>
+      <p className='font-semibold text-center'>This Header</p>
+      <div className='flex justify-between w-1/12'>
+        <Link href='/' className='bg-white text-blue-500 rounded'>
+          Home
+        </Link>
+        <Link href='/todos' className='bg-white text-blue-500 rounded'>
+          Todos
+        </Link>
+      </div>
+    </div>
+  )
 }
